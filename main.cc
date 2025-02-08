70cb5af55b1ac340009bdca536d09d691f0cae50
Victor Baldin <victor.baldin.2005@gmail.com>
2025-02-08 19:43:21 +0300

    Link against OpenCL successfully

---
+++ main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#ifndef CL_HPP_TARGET_OPENCL_VERSION
+#define CL_HPP_MINIMUM_OPENCL_VERSION 120
+#define CL_HPP_TARGET_OPENCL_VERSION 120
+#endif
+
+#define CL_HPP_CL_1_2_DEFAULT_BUILD
+#define CL_HPP_ENABLE_EXCEPTIONS
+
+#include "CL/opencl.hpp"
+
+int main() {
+  return 0;
+}
